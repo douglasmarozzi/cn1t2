2e486dc86867a214ae4309187ec0d3a332e83d38
douglasmarozzi <162488986+douglasmarozzi@users.noreply.github.com>
2024-03-11 15:47:18 -0300

    Update primeiro.py

--- primeiro.py
+++ primeiro.py
@@ -2,39 +2,33 @@ import streamlit as st
 import plotly.graph_objs as go
 import yfinance as yf
 
-st.title('Hello World')
+def main():
+    # Lista de bolsas disponíveis
+    bolsas_disponiveis = ["B3", "NASDAQ", "NYSE"]
 
-st.text('Inseria seu nome')
-nome = st.text_input('Nome')
+    # Perguntar ao usuário a bolsa e o nome da ação, data inicial e data final e plotar o gráfico do preço da ação no final do dia
+    st.sidebar.header('Configurações')
+    bolsa = st.sidebar.selectbox('Bolsa', bolsas_disponiveis)
+    acao = st.sidebar.text_input('Ação')
 
-st.write(f'Olá {nome}')
+    st.sidebar.text('Insira a data inicial')
+    data_inicial = st.sidebar.date_input('Data Inicial')
 
-def inverter_texto(texto):
-    return texto[::-1]
+    st.sidebar.text('Insira a data final')
+    data_final = st.sidebar.date_input('Data Final')
 
+    b1 = st.sidebar.button('Buscar')
+    if b1:
+        acao = yf.download(f"{acao}.{bolsa}", start=data_inicial, end=data_final)
+        fig = go.Figure(data=[go.Candlestick(x=acao.index,
+                                             open=acao['Open'],
+                                             high=acao['High'],
+                                             low=acao['Low'],
+                                             close=acao['Close'])])
 
-nome_invertido = inverter_texto(nome)
-st.write(f'Seu nome invertido é {nome_invertido}')
+        st.plotly_chart(fig)
+        # Printar o dataframe
+        st.write(acao)
 
-# perguntar ao usuario o nome da ação, data inicial e data final e plotar o grafico do preço da ação no final do dia
-st.text('Insira o nome da ação')
-acao = st.text_input('Ação') + '.SA'
-
-st.text('Insira a data inicial')
-data_inicial = st.date_input('Data Inicial')
-
-st.text('Insira a data final')
-data_final = st.date_input('Data Final')
-
-b1 = st.button('Buscar')
-if b1:
-    acao = yf.download(acao, start=data_inicial, end=data_final)
-    fig = go.Figure(data=[go.Candlestick(x=acao.index,
-                                      open=acao['Open'],
-                                      high=acao['High'],
-                                      low=acao['Low'],
-                                      close=acao['Close'])])
-
-    st.plotly_chart(fig)
-    # printar o dataframe
-    st.write(acao)
+if __name__ == "__main__":
+    main()
