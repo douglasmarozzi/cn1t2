b36f8aae2e37f22e00afb728e2c170994efb4722
douglasmarozzi <162488986+douglasmarozzi@users.noreply.github.com>
2024-03-11 15:46:04 -0300

    Update primeiro.py

--- primeiro.py
+++ primeiro.py
@@ -1,40 +1,48 @@
-import streamlit as st
-import plotly.graph_objs as go
-import yfinance as yf
 
-st.title('Hello World')
+@@ -2,23 +2,13 @@
+importar  plotadamente . graph_objs  em  andamento
+importar  yfinance  como  yf
 
-st.text('Inseria seu nome')
-nome = st.text_input('Nome')
+st . título ( 'Olá mundo' )
+# Lista de bolsas disponíveis
+bolsas_disponiveis  = [ "B3" , "NASDAQ" , "NYSE" ]
 
-st.write(f'Olá {nome}')
+st . text ( 'Inseria seu nome' )
+nome  =  st . text_input ( 'Nome' )
 
-def inverter_texto(texto):
-    return texto[::-1]
+st . escreva ( f'Olá { nome } ' )
+
+def  inversor_texto ( texto ):
+    retornar  texto [:: - 1 ]
 
 
-nome_invertido = inverter_texto(nome)
-st.write(f'Seu nome invertido é {nome_invertido}')
+nome_invertido  =  inverter_texto ( nome )
+st . write ( f'Seu nome invertido é { nome_invertido } ' )
 
-# perguntar ao usuario o nome da ação, data inicial e data final e plotar o grafico do preço da ação no final do dia
-st.text('Insira o nome da ação')
-acao = st.text_input('Ação') + '.SA'
+# perguntar ao usuário o nome da ação, dados iniciais e dados finais e plotar o gráfico do preço da ação no final do dia
+st . text ( 'Insira o nome da ação' )
+acao  =  st . text_input ( 'Ação' ) +  '.SA'
+# Pergunte ao usuário a bolsa e o nome da ação, dados iniciais e dados finais e plotar o gráfico do preço da ação no final do dia
+st . text ( 'Selecione a bolsa e insira o nome da ação' )
+bolsa  =  st . selectbox ( 'Bolsa' , bolsas_disponiveis )
+acao  =  st . text_input ( 'Ação' ) +  f'. { bolsa } '
 
-st.text('Insira a data inicial')
-data_inicial = st.date_input('Data Inicial')
+st . text ( 'Insira os dados iniciais' )
+dados_inicial  =  st . data_input ( 'Dados Iniciais' )
+@@ -30,11 +20,11 @@ def inverter_texto(texto):
+se  b1 :
+    acao  =  yf . baixar ( acao , início = dados_inicial , fim = dados_final )
+    figo  =  vai . Figura ( dados = [ go . Candlestick ( x = acao . index ,
+                                      open = acao [ 'Abrir' ],
+                                      alto = acao [ 'Alto' ],
+                                      baixo = acao [ 'Baixo' ],
+                                      fechar = acao [ 'Fechar' ])])
+                                         open = acao [ 'Abrir' ],
+                                         alto = acao [ 'Alto' ],
+                                         baixo = acao [ 'Baixo' ],
+                                         fechar = acao [ 'Fechar' ])])
 
-st.text('Insira a data final')
-data_final = st.date_input('Data Final')
-
-b1 = st.button('Buscar')
-if b1:
-    acao = yf.download(acao, start=data_inicial, end=data_final)
-    fig = go.Figure(data=[go.Candlestick(x=acao.index,
-                                      open=acao['Open'],
-                                      high=acao['High'],
-                                      low=acao['Low'],
-                                      close=acao['Close'])])
-
-    st.plotly_chart(fig)
-    # printar o dataframe
-    st.write(acao)
+    st . plotly_chart ( fig )
+    #imprime o dataframe
+    # Imprimir o dataframe
+    st . escreva ( açao )
