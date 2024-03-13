a00fc3d00ebd86d1daaf5d32acc97f825f8f073b
douglasmarozzi <162488986+douglasmarozzi@users.noreply.github.com>
2024-03-13 14:05:56 -0300

    Update primeiro.py

--- primeiro.py
+++ primeiro.py
@@ -4,21 +4,24 @@ import yfinance as yf
 
 st.title('Hello World')
 
-st.text('Inseria seu nome')
-nome = st.text_input('Nome')
-
-st.write(f'Olá {nome}')
-
+# Function to invert text
 def inverter_texto(texto):
     return texto[::-1]
 
+# Dictionary containing stock exchanges and their corresponding codes
+stock_exchanges = {
+    'Bovespa (Brazil)': 'SAO',
+    'New York Stock Exchange (USA)': 'NYQ',
+    'NASDAQ (USA)': 'NAS',
+    'London Stock Exchange (UK)': 'LON',
+    # Add more exchanges if needed
+}
 
-nome_invertido = inverter_texto(nome)
-st.write(f'Seu nome invertido é {nome_invertido}')
+st.text('Escolha a bolsa de valores:')
+selected_exchange = st.selectbox('Bolsa', list(stock_exchanges.keys()))
 
-# perguntar ao usuario o nome da ação, data inicial e data final e plotar o grafico do preço da ação no final do dia
 st.text('Insira o nome da ação')
-acao = st.text_input('Ação') + '.SA'
+acao = st.text_input('Ação') + '.' + stock_exchanges[selected_exchange]
 
 st.text('Insira a data inicial')
 data_inicial = st.date_input('Data Inicial')
@@ -30,11 +33,11 @@ b1 = st.button('Buscar')
 if b1:
     acao = yf.download(acao, start=data_inicial, end=data_final)
     fig = go.Figure(data=[go.Candlestick(x=acao.index,
-                                      open=acao['Open'],
-                                      high=acao['High'],
-                                      low=acao['Low'],
-                                      close=acao['Close'])])
+                                         open=acao['Open'],
+                                         high=acao['High'],
+                                         low=acao['Low'],
+                                         close=acao['Close'])])
 
     st.plotly_chart(fig)
-    # printar o dataframe
+    # Print the dataframe
     st.write(acao)
